3f4cc6f08aace20d7cbe0b22ba65528f2b16e15e
Deepak Mankotia <stratisky.deepakmankotia@gmail.com>
2020-04-10 14:48:22 +0530

    post page integration complete

---
+++ src/components/posts/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+import Header from "../../views/header";
+import Footer from "../../views/footer";
+import Register from "../../components/register";
+import Login from "../../components/login";
+import ForgetPassword from "../../components/forget-password";
+import Blog from "../../views/blog";
+
+class Posts extends React.Component {
+  state = {
+    registerPopUp: false,
+    loginPopUp: false,
+    forgetPasswordPopUp: false,
+  };
+
+  togglePopup = (e, popupType) => {
+    e.preventDefault();
+    this.setState({[popupType]: !this.state[popupType]});
+  };
+
+  openforgetpasswordpopup = (e) => {
+    e.preventDefault();
+    this.setState({forgetPasswordPopUp: true, loginPopUp: false});
+  };
+
+  openloginpopup = (e) => {
+    e.preventDefault();
+    this.setState({loginPopUp: true, registerPopUp: false});
+  };
+
+  render() {
+    const {registerPopUp, loginPopUp, forgetPasswordPopUp} = this.state;
+    return (
+      <>
+        <Header action={this.togglePopup} />
+        <div className="container container-posts">
+          <Blog />
+        </div>
+        <Footer />
+        {registerPopUp && (
+          <Register
+            action={this.togglePopup}
+            manageLogin={this.openloginpopup}
+          />
+        )}
+        {loginPopUp && (
+          <Login
+            action={this.togglePopup}
+            manageForgetPassword={this.openforgetpasswordpopup}
+          />
+        )}
+        {forgetPasswordPopUp && <ForgetPassword action={this.togglePopup} />}
+      </>
+    );
+  }
+}
+
+export default Posts;
---
+++ src/views/blog/single.js
@@ -0,0 +1,60 @@
+import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import Main from "./Main";
+import Sidebar from "./Sidebar";
+import post1 from "./blog-post.1.md";
+import post2 from "./blog-post.2.md";
+import post3 from "./blog-post.3.md";
+
+const useStyles = makeStyles((theme) => ({
+  mainGrid: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+const posts = [post1, post2, post3];
+
+const sidebar = {
+  title: "About",
+  description:
+    "Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.",
+  archives: [
+    {title: "March 2020", url: "#"},
+    {title: "February 2020", url: "#"},
+    {title: "January 2020", url: "#"},
+    ,
+  ],
+  social: [
+    {name: "Twitter", icon: TwitterIcon},
+    {name: "Facebook", icon: FacebookIcon},
+  ],
+};
+
+export default function Single() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <main>
+          <Grid container spacing={5} className={classes.mainGrid}>
+            <Main posts={posts} />
+            <Sidebar
+              title={sidebar.title}
+              description={sidebar.description}
+              archives={sidebar.archives}
+              social={sidebar.social}
+            />
+          </Grid>
+        </main>
+      </Container>
+    </React.Fragment>
+  );
+}
